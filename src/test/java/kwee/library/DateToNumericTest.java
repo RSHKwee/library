ee9f0cbf62819e5c9f45e3d371710492f5283fcf
PCPrive2017\René <rsh.kwee@gmail.com>
2023-05-26 16:34:26 +0200

    Test toegevoegd

--- src/test/java/kwee/library/DateToNumericTest.java
+++ src/test/java/kwee/library/DateToNumericTest.java
@@ -1,19 +1,36 @@
 package kwee.library;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import junit.framework.TestCase;
 
 public class DateToNumericTest extends TestCase {
 
+  @Override
   protected void setUp() throws Exception {
     super.setUp();
   }
 
+  @Override
   protected void tearDown() throws Exception {
     super.tearDown();
   }
 
   public void testDateToNumeric() {
-    fail("Not yet implemented");
+    SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+    try {
+      Date l_Date;
+      l_Date = formatter.parse("01-apr-2022");
+      String s_Date = DateToNumeric.dateToNumeric(l_Date);
+      assertEquals(s_Date, "20220401");
+    } catch (ParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
 }
