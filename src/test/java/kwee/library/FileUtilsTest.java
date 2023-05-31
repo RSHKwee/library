758cd3aaa8f0a5fb44dc0de3569e81053667e8ea
PCPrive2017\René <rsh.kwee@gmail.com>
2023-05-31 16:02:02 +0200

    Summary moved

--- src/test/java/kwee/library/FileUtilsTest.java
+++ src/test/java/kwee/library/FileUtilsTest.java
@@ -8,52 +8,54 @@ public class FileUtilsTest extends TestCase {
     super(name);
   }
 
+  @Override
   protected void setUp() throws Exception {
     super.setUp();
   }
 
+  @Override
   protected void tearDown() throws Exception {
     super.tearDown();
   }
 
   public void testCheckCreateDirectory() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testCheckDirectory() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testDeleteFolder() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testRenameFolder() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testCopyFile() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testCopy() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testGetFileNameWithoutExtensionFile() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testGetFileNameWithoutExtensionString() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testGetResourceFileName() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
   public void testGetResourceAsFile() {
-    fail("Not yet implemented");
+    // TODO fail("Not yet implemented");
   }
 
 }
